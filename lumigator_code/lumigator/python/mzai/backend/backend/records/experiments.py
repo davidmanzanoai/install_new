33c89970f022ec1cdddc58fcb5dd0da48f6c4b5f
David Manzano <davidmanzano@Davids-MacBook-MZAI.local>
2025-02-27 23:56:03 +0100

    nueva version

--- lumigator_code/lumigator/python/mzai/backend/backend/records/experiments.py
+++
@@ -1,6 +0,0 @@
-from backend.records.base import BaseRecord
-from backend.records.mixins import DateTimeMixin, JobStatusMixin, NameDescriptionMixin
-
-
-class ExperimentRecord(BaseRecord, NameDescriptionMixin, JobStatusMixin, DateTimeMixin):
-    __tablename__ = "experiments"
